5caa49c23c999938e28d7c68552db21afaf4abca
Diego <Diego@Diego>
2016-12-12 09:50:45 +0100

    Instanciado objeto y llamado el método mostrar().

--- src/factorial/Factorial.java
+++ src/factorial/Factorial.java
@@ -7,9 +7,8 @@ public class Factorial {
 
     public static void main(String[] args) {
         
-        
-
-
+        Metodos calcular = new Metodos();//Objeto de la clase Metodos.
+        calcular.mostrar();//Llamando al método mostrar().
     }
     
 }
